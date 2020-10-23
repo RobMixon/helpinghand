7a221fc36060f1f4fdfd984ffc504de0d882f79e
Rob Mixon <mixonroberth@gmail.com>
2020-10-23 09:56:24 -0500

    adding gitignore and firebase id

---
+++ helpinghand/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        UserProfile GetByFirebaseUserId(string firebaseUserId);
+        List<UserProfile> ListAllUserProfiles();
+    }
+}
